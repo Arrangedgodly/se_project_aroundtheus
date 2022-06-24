a4bcb61042b78d01edfb77ba9c4ed1f60098eddf
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-06-24 11:30:21 -0600

    create array of card data

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "Venice",
+    link: "https://images.unsplash.com/photo-1654957889404-f16aa636cf49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+  },
+  {
+    name: "Gold Coast",
+    link: "https://images.unsplash.com/photo-1652734303055-da747a7aedd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"
+  },
+  {
+    name: "Los Angeles",
+    link: "https://images.unsplash.com/photo-1654543681412-bee75ad60897?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+  },
+  {
+    name: "Ibiza",
+    link: "https://images.unsplash.com/photo-1653596010737-1460ad607724?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+  },
+  {
+    name: "Tokyo",
+    link: "https://images.unsplash.com/photo-1648301184879-28c6ed4964d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+  },
+  {
+    name: "Australia",
+    link: "https://images.unsplash.com/photo-1583790773386-5c73e9b37d8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+  }
+]
