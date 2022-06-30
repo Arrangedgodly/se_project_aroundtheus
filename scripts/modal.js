86ba1fae25c5c4a99cde8280c05fdb991a853f39
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-06-29 20:03:13 -0600

    added modal opening script

---
+++ scripts/modal.js
@@ -0,0 +1,10 @@
+const editButton = document.querySelector(".button_type_edit");
+const closeButton = document.querySelector(".button_type_close");
+const modal = document.querySelector(".modal");
+
+function toggleModal() {
+  modal.classList.toggle("modal__opened");
+}
+
+editButton.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
