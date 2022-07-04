58f4870068833ccb732754a2dd7e28a7232e6c01
Arrangedgodly <arrangedgodly@gmail.com>
2022-07-03 19:44:39 -0600

    refactor: still testing how to fix the modal section

--- scripts/profile-modal.js
+++ scripts/profile-modal.js
@@ -1,6 +1,6 @@
 const editProfileButton = document.querySelector(".button_type_edit");
 const closeProfileModalButton = document.querySelector(".button_type_close");
-//const profileModal = document.querySelector(".profile-modal");
+const profileModal = document.querySelector(".profile-modal");
 
 editProfileButton.addEventListener("click", openModal(profileModal));
 closeProfileModalButton.addEventListener("click", closeModal(profileModal));
