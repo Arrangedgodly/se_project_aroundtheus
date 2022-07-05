838f704ce001854b09b7c46087b58105251618b3
Arrangedgodly <arrangedgodly@gmail.com>
2022-07-05 10:08:33 -0600

    refactor(profile-modal): reworked profile-modal.js to add functions for the profilemodal specifically, fixing the error inside of the page preventing the modal from opening entirely

--- scripts/profile-modal.js
+++ scripts/profile-modal.js
@@ -2,5 +2,13 @@ const editProfileButton = document.querySelector(".button_type_edit");
 const closeProfileModalButton = document.querySelector(".button_type_close");
 const profileModal = document.querySelector(".profile-modal");
 
-editProfileButton.addEventListener("click", openModal(profileModal));
-closeProfileModalButton.addEventListener("click", closeModal(profileModal));
+function openProfileModal() {
+    openModal(profileModal);
+}
+
+function closeProfileModal() {
+    closeModal(profileModal);
+}
+
+editProfileButton.addEventListener("click", openProfileModal);
+closeProfileModalButton.addEventListener("click", closeProfileModal);
