8150bf2a5004e9ae5374b5b2484944c2cd7b2ff2
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-07-10 22:51:36 -0600

    feat: finished through step 5, added like button fill and added trash icon to delete new cards

--- scripts/card.js
+++ scripts/card.js
@@ -1,10 +1,27 @@
 const cardTemplate = document.querySelector("#card").content;
 const cards = document.querySelector(".cards");
 
+function toggleCardLikeButtonFill(button) {
+  button.classList.toggle("button_type_like_filled");
+}
+
 function createCard(data) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardElementImage = cardElement.querySelector(".card__image");
   const cardElementHeader = cardElement.querySelector(".card__footer-title");
+
+  const cardTrashButton = cardElement.querySelector(".button_type_trash");
+  function removeCard() {
+    cardElement.remove();
+  }
+  cardTrashButton.addEventListener("click", removeCard);
+
+  const cardLikeButton = cardElement.querySelector(".button_type_like");
+  function changeCardLikeButtonFill() {
+    toggleCardLikeButtonFill(cardLikeButton);
+  }
+  cardLikeButton.addEventListener("click", changeCardLikeButtonFill);
+
   const cardTitle = data.name;
   const cardLink = data.link;
   cardElementImage.setAttribute("src", cardLink);
