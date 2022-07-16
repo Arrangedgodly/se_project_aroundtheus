b4033034b0dfa8d76b3d6091bf7a587d90329aea
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-07-16 14:13:11 -0600

    refactor: changed profile-modal to take current name and desc for values on the form

--- scripts/profile-modal.js
+++ scripts/profile-modal.js
@@ -2,17 +2,9 @@ const editProfileButton = document.querySelector(".button_type_edit");
 const profileModal = document.querySelector(".profile-modal");
 const closeProfileModalButton = profileModal.querySelector(".button_type_close");
 
-function createProfileModal() {
-  const profileModalHeader = profileModal.querySelector(".form__header");
-  profileModalHeader.textContent = "Edit profile";
-  const profileModalInputName = profileModal.querySelector(".form__input_type_name");
-  profileModalInputName.setAttribute("placeholder", "Set Your New Name");
-  const profileModalInputDesc = profileModal.querySelector(".form__input_type_desc");
-  profileModalInputDesc.setAttribute("placeholder", "Set Your New Job Title");
-  return profileModal;
-}
-
 function openProfileModal() {
+    profileNameInput.value = profileName.textContent;
+    profileDescInput.value = profileDesc.textContent;
     openModal(profileModal);
 }
 
